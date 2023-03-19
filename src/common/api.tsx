049a94b6fad1508386d2d2f879b60905621a3367
nostrss <jintagi@gmail.com>
2023-03-19 20:49:51 +0900

    api url domain fix

--- src/common/api.tsx
+++ src/common/api.tsx
@@ -16,7 +16,7 @@ export const fetchDataListPage = async (page: number) => {
   const url =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/api/lists'
-      : '/api/lists';
+      : 'https://nextjs-blog-black-pi.vercel.app/api/lists';
   try {
     const response = await axios.get(`${url}/${page}`);
     return response;
