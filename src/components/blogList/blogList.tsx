910551d4f19d5e21e2ced32df4527debbe46b384
nostrss <jintagi@gmail.com>
2023-04-03 00:36:46 +0900

    infinite scroll with firebase

--- src/components/blogList/blogList.tsx
+++ src/components/blogList/blogList.tsx
@@ -5,35 +5,53 @@ import { useEffect, useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { v4 as uuidv4 } from 'uuid';
 import { firebaseDb } from 'firebase.config';
-import { DocumentData, collection, getDocs } from 'firebase/firestore';
+import {
+  DocumentData,
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from 'firebase/firestore';
 import BlogListItem from './blogListItem/blogListItem';
 
 export default function BlogList() {
   const [blogLists, setBlogLists] = useState<DocumentData[]>([]);
-  const [totalPage, setTotalPage] = useState(0);
-  const [currentPage, setCurrentPage] = useState(0);
+  const [isLastVisible, setIsLastVisible] = useState<DocumentData | null>(null);
 
   const fetchPostList = async () => {
+    const isFirstLoading = blogLists.length === 0;
+
+    const fetchQuery = isFirstLoading
+      ? query(
+          collection(firebaseDb, 'post'),
+          orderBy('createdAt', 'desc'),
+          limit(5),
+        )
+      : query(
+          collection(firebaseDb, 'post'),
+          orderBy('createdAt', 'desc'),
+          startAfter(isLastVisible),
+          limit(5),
+        );
+
     let tmpList: DocumentData[] = [];
-    const querySnapshot = await getDocs(collection(firebaseDb, 'post'));
-    querySnapshot.forEach((doc) => {
-      tmpList = [...tmpList, doc.data()];
-    });
-
-    setBlogLists(tmpList);
-    setTotalPage(tmpList.length);
-    setCurrentPage(currentPage + 1);
+    await getDocs(fetchQuery)
+      .then((querySnapshotData) => {
+        setIsLastVisible(
+          querySnapshotData.docs[querySnapshotData.docs.length - 1],
+        );
+        querySnapshotData.forEach((doc) => {
+          tmpList = [...tmpList, doc.data()];
+        });
+        setBlogLists([...blogLists, ...tmpList]);
+        return querySnapshotData;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
-
-  // const fetchNextPage = async () => {
-  //   const result = await fetchDataListPerPage(currentPage + 1);
-  //   const { data } = result as {
-  //     data: { total: number; bloglistData: BlogPost[] };
-  //   };
-  //   setTotalPage(data.total);
-  //   setBlogLists([...blogLists, ...data.bloglistData]);
-  //   setCurrentPage(currentPage + 1);
-  // };
 
   useEffect(() => {
     fetchPostList();
@@ -41,7 +59,7 @@ export default function BlogList() {
 
   return (
     <>
-      <h1>블로그 무한스크롤 페이지입니다.</h1>
+      <h1>블로그 무한스크롤 페이지입니다</h1>
       <p>
         페이지네이션으로 보기
         <Link href="/page/1">
@@ -52,7 +70,7 @@ export default function BlogList() {
         <InfiniteScroll
           dataLength={blogLists.length}
           next={fetchPostList}
-          hasMore={totalPage !== blogLists.length}
+          hasMore={!!isLastVisible}
           loader={<h4>Loading...</h4>}
           endMessage={
             <p style={{ textAlign: 'center' }}>
--- src/components/blogList/blogListItem/blogListItem.tsx
+++ src/components/blogList/blogListItem/blogListItem.tsx
@@ -1,13 +1,19 @@
 import Link from 'next/link';
+import parse from 'node-html-parser';
 
 export default function BlogListItem({ blogListItem }: any) {
+  const plainContents = parse(blogListItem.contents).text;
+
+  const date = new Date(
+    blogListItem.createdAt.seconds * 1000,
+  ).toLocaleDateString('ko-KR');
   return (
     <article>
       <Link href={`/detail/${blogListItem.postId}`}>
         <li>
           <h2>{blogListItem.title}</h2>
-          <p>{blogListItem.contents}</p>
-          <p>{blogListItem.timestamp}</p>
+          <p>{plainContents}</p>
+          <p>{date}</p>
           <button type="button">Like</button>
         </li>
       </Link>
